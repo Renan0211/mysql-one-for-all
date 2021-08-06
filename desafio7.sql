7963d8fac33a2aab128e5274e324282daed3ac71
Renan0211 <renan.antunes.rocha@gmail.com>
2021-08-06 14:40:41 -0300

    resolve o desafio 7

---
+++ desafio7.sql
@@ -0,0 +1,10 @@
+CREATE VIEW SpotifyClone.perfil_artistas AS
+    SELECT 
+       CONCAT(a.first_name, ' ', a.last_name) AS artista,
+       a2.album_title AS album,
+       COUNT(ua.artist_id) AS seguidores
+       FROM artists AS a
+       INNER JOIN albums AS a2 ON a.artist_id = a2.artist_id
+       INNER JOIN user_artist AS ua ON a.artist_id = ua.artist_id
+       GROUP BY artista, album
+       ORDER BY seguidores DESC, artista ASC, album ASC;
