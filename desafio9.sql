478f9c3aa427a71d0640397d67690bd091c0a7b1
Renan0211 <renan.antunes.rocha@gmail.com>
2021-08-06 16:18:30 -0300

    resolve o desafio 9

---
+++ desafio9.sql
@@ -0,0 +1,15 @@
+DELIMITER $$
+
+CREATE PROCEDURE SpotifyClone.albuns_do_artista(IN artist_name VARCHAR(100))
+BEGIN
+	SELECT 
+    CONCAT(a.first_name, ' ', a.last_name) AS artista,
+    a2.album_title AS album
+    FROM artists AS a
+    INNER JOIN albums AS a2 ON a.artist_id = a2.artist_id
+    WHERE CONCAT(a.first_name, ' ', a.last_name) = artist_name
+    GROUP BY artista, album
+    ORDER BY album ASC;
+END $$
+
+DELIMITER ;
