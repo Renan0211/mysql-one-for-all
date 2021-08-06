b182e8d80c3e871c20a342bb71aaed6fc12d6a2b
Renan0211 <renan.antunes.rocha@gmail.com>
2021-08-06 13:43:22 -0300

    resolve o desafio 3

---
+++ desafio3.sql
@@ -0,0 +1,12 @@
+USE SpotifyClone;
+
+CREATE VIEW historico_reproducao_usuarios AS
+    SELECT 
+    u.user_name AS usuario, s.song_name AS nome
+FROM
+    users AS u
+        INNER JOIN
+    user_historic AS uh ON uh.user_id = u.user_id
+        INNER JOIN
+    songs AS s ON uh.song_id = s.song_id
+ORDER BY u.user_name ASC , s.song_name ASC;
